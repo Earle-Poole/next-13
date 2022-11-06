3e5a79ab006ea44eab67b3d4a6bb8f2112f2b3e7
Earle-Poole <eepoole4@gmail.com>
2022-11-05 19:52:47 -0500

    Quick SEO patches

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
       <head key={'root-layout'}>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Next 13 Practice</title>
       </head>
       <body className='bg-black text-gray-100 min-h-screen'>{children}</body>
