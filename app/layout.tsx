babf422315add791caa600928f2ba47a507fec07
Earle-Poole <eepoole4@gmail.com>
2022-11-05 19:57:32 -0500

    Quick SEO patches

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     <html>
       <head key={'root-layout'}>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='A test of new Next 13 paradigms.' />
         <title>Next 13 Practice</title>
       </head>
       <body className='bg-black text-gray-100 min-h-screen'>{children}</body>
