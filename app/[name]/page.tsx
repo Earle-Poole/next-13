3a09a09da3d5bd72ce7c62ea699ca20a8eef2212
Earle-Poole <eepoole4@gmail.com>
2022-11-06 20:05:00 -0600

    Improved header and error. Restructured files

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,11 +1,6 @@
-import {
-  getPokemonByName,
-  getPokemonList,
-  inchesToFeetAndInches,
-} from '../../utils/lib'
-import capitalize from 'lodash/capitalize'
-import Image from 'next/image'
+import { getPokemonByName, getPokemonList } from '../../utils/lib'
 import PokemonButton from '../components/atom/PokemonButton'
+import PokemonCard from '../components/organisms/PokemonCard'
 
 export default async function Page({
   params: { name },
@@ -14,42 +9,10 @@ export default async function Page({
 }) {
   const pokemon = await getPokemonByName(name)
   const pokemonList = await getPokemonList()
+
   return (
-    <div className='min-h-screen flex justify-center items-center flex-col'>
-      <div className='flex flex-col mb-4 justify-center items-center'>
-        <section className='flex'>
-          {pokemon.sprites.back_default ? (
-            <Image
-              style={{ imageRendering: 'pixelated' }}
-              src={pokemon.sprites.back_default}
-              height='280'
-              width='280'
-              priority
-              alt=''
-            />
-          ) : null}
-          {pokemon.sprites.front_default ? (
-            <Image
-              style={{ imageRendering: 'pixelated' }}
-              src={pokemon.sprites.front_default}
-              height='280'
-              width='280'
-              priority
-              alt=''
-            />
-          ) : null}
-        </section>
-        <h1 className='text-2xl font-bold'>{capitalize(pokemon.name)}</h1>
-        <h2 className='text-xl font-bold'>
-          Height: {inchesToFeetAndInches(Math.round(pokemon.height * 3.93701))}
-        </h2>
-        <h2 className='text-xl font-bold'>
-          Weight: {Math.round(pokemon.weight * 0.220462)} lbs
-        </h2>
-        <h2 className='text-xl font-bold'>
-          Type: {pokemon.types.map((t) => capitalize(t.type.name)).join(', ')}
-        </h2>
-      </div>
+    <div className='flex-1 flex justify-center items-center flex-col'>
+      <PokemonCard pokemon={pokemon} />
       <PokemonButton pokemonList={pokemonList} />
     </div>
   )
