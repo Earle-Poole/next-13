c19199ea7732eb50939f18bcee0fcf65f8980564
Earle Poole <44408506+Earle-Poole@users.noreply.github.com>
2022-11-07 10:24:51 -0600

    Update Header.tsx
    Nix the red border

--- app/components/organisms/Header/Header.tsx
+++ app/components/organisms/Header/Header.tsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <nav className='border-4 border-slate-800 bg-slate-700 min-h-[4rem] max-h-[4rem] flex text-xl font-semibold gap-4 p-2 mb-8'>
       <SectionOne />
-      <section className='flex-1 border-2 border-red-600'></section>
-      <section className='flex-1 border-2 border-red-600'></section>
-      <section className='flex-1 border-2 border-red-600'></section>
+      <section className='flex-1'></section>
+      <section className='flex-1'></section>
+      <section className='flex-1'></section>
     </nav>
   )
 }
