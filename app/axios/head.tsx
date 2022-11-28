ecaf63763ee82b76a940856e7148284805bee1d2
Earle-Poole <eepoole4@gmail.com>
2022-11-27 23:21:57 -0600

    Add meta tag

--- app/axios/head.tsx
+++ app/axios/head.tsx
@@ -5,6 +5,7 @@
 const NextHead = () => {
   return (
     <>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta name='description' content='Nested layouts in Next 13' />
       <title>Next 13 - Axios</title>
     </>
