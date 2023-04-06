23cb9c38e11ca9eab751f6ed41a2fd85c885430c
Earle Poole <eepoole4@gmail.com>
2023-04-06 01:21:08 -0500

    Added markdown rendering and code syntax highlighting

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,6 +4,8 @@ import ChatOutput from "@/components/atom/ChatOutput/ChatOutput"
 const Page = async () => {
   return (
     <div className="w-screen flex flex-1 flex-col gap-4 p-8 max-w-4xl">
+      {/**Wrap these in a component that will handle the logic of `isWaiting`,
+      instead of the atomWithStorage, maybe a template?*/}
       <ChatOutput />
       <ChatInput />
     </div>
--- app/components/atom/ChatOutput/ChatOutput.tsx
+++ app/components/atom/ChatOutput/ChatOutput.tsx
@@ -2,13 +2,18 @@
 import { useAtom } from "jotai"
 import { chatAtom } from "@/components/stores/ChatStore"
 import capitalize from "lodash/capitalize"
-import { useEffect, useRef, useState } from "react"
+import { lazy, useEffect, useRef, useState } from "react"
 import LoadingIndicator from "../LoadingIndicator/LoadingIndicator"
+import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import classNames from "classnames"
+import { ChatCompletionResponseMessageRoleEnum } from "openai"
+
+const ReactMarkdown = lazy(() => import("react-markdown"))
+const SyntaxHighlighter = lazy(
+  () => import("react-syntax-highlighter/dist/esm/prism")
+)
 
 const ChatOutput = () => {
-  // `isMounted` is used to prevent the component from rendering before the
-  // `useEffect` hook has been called. This prevents the component from
-  // rendering with default data on the server.
   const [isMounted, setIsMounted] = useState(false)
   const [{ messages, isWaiting }] = useAtom(chatAtom)
   const chatOutputRef = useRef<HTMLElement>(null)
@@ -35,11 +40,66 @@ const ChatOutput = () => {
         <>
           {messages.map((message) => (
             <div key={message.content}>
-              <article className="pb-2">
-                <label className="text-sm italic pb-1">
+              <article
+                className={classNames(
+                  "px-4 py-2 rounded mr-4 mb-2 [&>p]:mb-2",
+                  {
+                    "bg-white/10":
+                      message.role ===
+                      ChatCompletionResponseMessageRoleEnum.Assistant,
+                  }
+                )}
+              >
+                <label className="text-sm italic mb-4">
                   {capitalize(message.role)}:
                 </label>
-                <p>{message.content}</p>
+                <ReactMarkdown
+                  components={{
+                    // This could use more components to fully support markdown
+                    h1: ({ children }) => (
+                      <h1 className="text-4xl font-normal leading-normal mt-0 mb-2">
+                        {children}
+                      </h1>
+                    ),
+                    h2: ({ children }) => (
+                      <h2 className="text-3xl font-normal leading-normal mt-0 mb-2">
+                        {children}
+                      </h2>
+                    ),
+                    h3: ({ children }) => (
+                      <h3 className="text-2xl font-normal leading-normal mt-0 mb-2">
+                        {children}
+                      </h3>
+                    ),
+                    code: ({ inline, children, className, node, ...props }) => {
+                      const hasPosition = node.children[0].position
+                      const match = /language-(\w+)/.exec(className || "")
+
+                      return (!inline && match) || !hasPosition ? (
+                        <SyntaxHighlighter
+                          {...props}
+                          children={String(children).replace(/\n$/, "")}
+                          style={darcula}
+                          className={classNames(
+                            "w-fit rounded !m-4",
+                            className
+                          )}
+                          language={match?.[1]}
+                          PreTag="div"
+                        />
+                      ) : (
+                        <code
+                          {...props}
+                          className={"bg-[rgb(43,43,43)] py-1 px-2 rounded"}
+                        >
+                          {children}
+                        </code>
+                      )
+                    },
+                  }}
+                >
+                  {message.content}
+                </ReactMarkdown>
               </article>
             </div>
           ))}
