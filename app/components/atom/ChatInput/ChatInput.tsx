2029c8c462af4c406ad20f3512ba952d7472053a
Earle Poole <eepoole4@gmail.com>
2023-04-07 09:28:40 -0500

    Added some mobile styles. Fixed an a11y conflict. Implemented first pass of `chat-stream` end point

--- app/components/atom/ChatInput/ChatInput.tsx
+++ app/components/atom/ChatInput/ChatInput.tsx
@@ -14,12 +14,18 @@ import {
   ChatCompletionRequestMessageRoleEnum,
   ChatCompletionResponseMessage,
 } from "openai"
-import { FormEventHandler, useEffect, useState } from "react"
+import { FormEventHandler, useEffect, useRef, useState } from "react"
 import Select from "react-select"
 import LoadingIndicator from "../LoadingIndicator/LoadingIndicator"
 import { encodeBase64 } from "@/utils/lib"
+import { fetchEventSource } from "@microsoft/fetch-event-source"
+import { HEADERS_STREAM } from "pages/api/chat-stream"
+
+class RetriableError extends Error {}
+class FatalError extends Error {}
 
 const ChatInput = () => {
+  const answerNode = useRef<HTMLElement>(null)
   const [isMounted, setIsMounted] = useState(false)
   const [{ messages, model, isWaiting }, setChatStore] = useAtom(chatAtom)
 
@@ -79,6 +85,73 @@ const ChatInput = () => {
 
     setIsWaiting(true)
     try {
+      const ctrl = new AbortController()
+      fetchEventSource("/api/chat-stream", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestObject),
+        openWhenHidden: true,
+        signal: ctrl.signal,
+        async onopen(response) {
+          // TODO: Reset the answer node
+          if (answerNode.current) {
+            answerNode.current.innerText = ""
+          }
+          console.log("onopen")
+          if (
+            response.ok &&
+            response.headers.get("content-type")?.replace(/ /g, "") ===
+              HEADERS_STREAM["Content-Type"]
+          ) {
+            // The stream opened successfully
+            return
+          } else if (
+            response.status >= 400 &&
+            response.status < 500 &&
+            response.status !== 429
+          ) {
+            // client-side errors are usually non-retriable:
+            throw new FatalError()
+          } else {
+            throw new RetriableError()
+          }
+        },
+        onmessage(msg) {
+          // TODO: If the server emits an error message, throw an exception
+          // so it gets handled by the onerror callback below:
+          if (msg.event === "FatalError") {
+            throw new FatalError(msg.data)
+          }
+          try {
+            console.log("msg.data: ", msg.data)
+          } catch (error) {
+            console.log("aborting")
+            ctrl.abort()
+            // onClose()
+          }
+        },
+        onclose() {
+          // TODO: If the server closes the connection unexpectedly, retry:
+          // throw new RetriableError()
+          // onClose()
+        },
+        onerror(err: Error) {
+          if (err instanceof Error) {
+            console.log("onerror fatal", err)
+            // rethrow to stop the operation
+            // setAwaitingFirstToken(false)
+            // setStreaming(false)
+            // setError(`Something went wrong with the request`)
+            // throw err
+          } else {
+            console.log("onerror other", err)
+            // do nothing to automatically retry. You can also
+            // return a specific retry interval here.
+          }
+        },
+      })
       const res = await sendChatCompletionRequest(encrypted)
 
       if (!res.choices[0].message) {
@@ -117,35 +190,39 @@ const ChatInput = () => {
 
   return (
     <form onSubmit={onSubmit} className="flex gap-2 w-full">
-      {isMounted ? (
-        <Select
-          className="text-black min-w-[14rem] min-h-[2.75rem]"
-          classNames={{
-            control: () => "!min-h-[2.75rem]",
-          }}
-          menuPlacement="top"
-          options={Object.values(ChatModels).map((chatModel) => ({
-            value: chatModel,
-            label: chatModel,
-          }))}
-          defaultValue={{ value: model, label: model }}
-          onChange={onModelChange}
+      <div className="flex flex-1 flex-wrap gap-2">
+        {isMounted ? (
+          <Select
+            className="text-black min-w-[13rem] min-h-[2.75rem]"
+            classNames={{
+              control: () => "!min-h-[2.75rem]",
+            }}
+            menuPlacement="top"
+            options={Object.values(ChatModels).map((chatModel) => ({
+              value: chatModel,
+              label: chatModel,
+            }))}
+            defaultValue={{ value: model, label: model }}
+            onChange={onModelChange}
+          />
+        ) : (
+          <div className="flex justify-center items-center min-w-[13rem] min-h-[2.75rem]">
+            <LoadingIndicator />
+          </div>
+        )}
+        <input
+          placeholder="Send a message..."
+          className="text-black p-2 rounded flex-1"
+          name="message"
+          type="text"
         />
-      ) : (
-        <div className="flex justify-center items-center min-w-[14rem] min-h-[2.75rem]">
-          <LoadingIndicator />
-        </div>
-      )}
-      <input
-        placeholder="Send a message..."
-        className="text-black p-2 rounded flex-1"
-        name="message"
-        type="text"
-      />
-      <Button type="submit">Send</Button>
-      <Button type="button" onClick={onClear}>
-        Clear
-      </Button>
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Button type="submit">Send</Button>
+        <Button type="button" onClick={onClear}>
+          Clear
+        </Button>
+      </div>
     </form>
   )
 }
