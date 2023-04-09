743c0d2e19aaf7c7855d9f934d2b006d738740e4
Earle Poole <eepoole4@gmail.com>
2023-04-09 16:54:34 -0500

    Unused imports

--- app/components/molecules/MarkdownWrapper/MarkdownWrapper.tsx
+++ app/components/molecules/MarkdownWrapper/MarkdownWrapper.tsx
@@ -1,6 +1,4 @@
-import classNames from "classnames"
 import { FC, PropsWithChildren, lazy } from "react"
-import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 import markdownWrapperComponents from "./MarkdownWrapper.Components"
 const ReactMarkdown = lazy(() => import("react-markdown"))
 
