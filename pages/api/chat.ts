4906c5abf676f9894ea67df95b744bf0fc5acf5d
Earle Poole <eepoole4@gmail.com>
2023-04-20 22:42:25 -0500

    lint styles and pages directories

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,17 +1,17 @@
-import { ChatModels } from "@/components/stores/ChatStore"
+import { ChatModels } from '@/components/stores/ChatStore'
 import {
-  TextModelValues,
-  TextModels,
-} from "@/components/stores/TextCompleteStore"
-import { decodeBase64, encodeBase64 } from "@/utils/lib"
-import { NextApiRequest, NextApiResponse } from "next"
-import { Configuration, OpenAIApi } from "openai"
-import { OpenAIApi as EdgeOpenAIApi } from "openai-edge"
-import { ChatModelValues } from "types/useChat.types"
+    TextModelValues,
+    TextModels,
+} from '@/components/stores/TextCompleteStore'
+import { decodeBase64, encodeBase64 } from '@/utils/lib'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai'
+import { OpenAIApi as EdgeOpenAIApi } from 'openai-edge'
+import { ChatModelValues } from 'types/useChat.types'
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_SECRET,
-  organization: "org-oxy4ydBZDsiyT71nD4KiFVBC",
+    apiKey: process.env.OPENAI_SECRET,
+    organization: 'org-oxy4ydBZDsiyT71nD4KiFVBC',
 })
 
 const openai = new OpenAIApi(configuration)
@@ -23,79 +23,84 @@ const edgeopenai = new EdgeOpenAIApi(configuration)
  * @param res
  */
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    const enginesResponse = await openai.listEngines()
-    const { data: enginesData } = enginesResponse
+    if (req.method === 'GET') {
+        const enginesResponse = await openai.listEngines()
+        const { data: enginesData } = enginesResponse
 
-    return new Response(JSON.stringify(enginesData), { status: 200 })
-  }
-
-  const body = await new Response(req.body).json()
-  const decodedData = decodeBase64(body.data)
-  const parsedBody =
-    typeof body.data === "string" ? JSON.parse(decodedData) : {}
-
-  if (req.method !== "POST") {
-    return new Response("Invalid request method, please use POST.", {
-      status: 400,
-    })
-  }
-
-  if (!("model" in parsedBody) || typeof parsedBody.model !== "string") {
-    return new Response("Missing model parameter", { status: 400 })
-  }
-
-  const parsedBodyModel: ChatModelValues | TextModelValues = parsedBody.model
-  try {
-    const isTextCompletionRequest = Object.values(TextModels).includes(
-      parsedBodyModel as TextModelValues
-    )
-
-    if (isTextCompletionRequest) {
-      const textCompletionResponse = await (
-        await edgeopenai.createCompletion({
-          model: parsedBodyModel,
-          prompt: "Respond to this prompt with a short story.",
-          max_tokens: 2048,
-          n: 1,
-        })
-      ).json()
-
-      return new Response(JSON.stringify(textCompletionResponse), {
-        status: 200,
-      })
+        return new Response(JSON.stringify(enginesData), { status: 200 })
     }
 
-    const isChatCompletionRequest = Object.values(ChatModels).includes(
-      parsedBodyModel as ChatModelValues
-    )
+    const body = await new Response(req.body).json()
+    const decodedData = decodeBase64(body.data)
+    const parsedBody =
+        typeof body.data === 'string' ? JSON.parse(decodedData) : {}
 
-    if (isChatCompletionRequest) {
-      const chatCompletionResponse = await (
-        await edgeopenai.createChatCompletion({
-          model: parsedBodyModel,
-          temperature: 0.7,
-          messages: parsedBody.messages,
+    if (req.method !== 'POST') {
+        return new Response('Invalid request method, please use POST.', {
+            status: 400,
         })
-      ).json()
-
-      const encrypted = encodeBase64(JSON.stringify(chatCompletionResponse))
-      return new Response(encrypted, { status: 200 })
     }
-  } catch (e) {
-    // TODO: Properly handle errors, pop off last user message, or add a message to the chat bot
-    console.error("Could not access OpenAI, please try again.\n\nError: ", e)
-    return new Response("Could not access OpenAI, please try again.", {
-      status: 400,
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-  }
+
+    if (!('model' in parsedBody) || typeof parsedBody.model !== 'string') {
+        return new Response('Missing model parameter', { status: 400 })
+    }
+
+    const parsedBodyModel: ChatModelValues | TextModelValues = parsedBody.model
+    try {
+        const isTextCompletionRequest = Object.values(TextModels).includes(
+            parsedBodyModel as TextModelValues
+        )
+
+        if (isTextCompletionRequest) {
+            const textCompletionResponse = await (
+                await edgeopenai.createCompletion({
+                    model: parsedBodyModel,
+                    prompt: 'Respond to this prompt with a short story.',
+                    max_tokens: 2048,
+                    n: 1,
+                })
+            ).json()
+
+            return new Response(JSON.stringify(textCompletionResponse), {
+                status: 200,
+            })
+        }
+
+        const isChatCompletionRequest = Object.values(ChatModels).includes(
+            parsedBodyModel as ChatModelValues
+        )
+
+        if (isChatCompletionRequest) {
+            const chatCompletionResponse = await (
+                await edgeopenai.createChatCompletion({
+                    model: parsedBodyModel,
+                    temperature: 0.7,
+                    messages: parsedBody.messages,
+                })
+            ).json()
+
+            const encrypted = encodeBase64(
+                JSON.stringify(chatCompletionResponse)
+            )
+            return new Response(encrypted, { status: 200 })
+        }
+    } catch (e) {
+        // TODO: Properly handle errors, pop off last user message, or add a message to the chat bot
+        console.error(
+            'Could not access OpenAI, please try again.\n\nError: ',
+            e
+        )
+        return new Response('Could not access OpenAI, please try again.', {
+            status: 400,
+            headers: {
+                'content-type': 'application/json',
+            },
+        })
+    }
 }
 
 export default handler
 
 export const config = {
-  runtime: "edge",
+    runtime: 'edge',
 }
--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,61 +1,61 @@
-import { ImageSizes } from "@/utils/hooks/useImage"
-import type { NextRequest } from "next/server"
-import { Configuration, OpenAIApi } from "openai-edge"
+import { ImageSizes } from '@/utils/hooks/useImage'
+import type { NextRequest } from 'next/server'
+import { Configuration, OpenAIApi } from 'openai-edge'
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_SECRET,
+    apiKey: process.env.OPENAI_SECRET,
 })
 const openai = new OpenAIApi(configuration)
 
 const handler = async (req: NextRequest) => {
-  if (req.method !== "POST") {
-    return new Response("Invalid request method, please use POST", {
-      status: 400,
-    })
-  }
-  const body = await new Response(req.body).json()
+    if (req.method !== 'POST') {
+        return new Response('Invalid request method, please use POST', {
+            status: 400,
+        })
+    }
+    const body = await new Response(req.body).json()
 
-  if (!("message" in body) || typeof body.message !== "string") {
-    return new Response("Missing message parameter", { status: 400 })
-  }
+    if (!('message' in body) || typeof body.message !== 'string') {
+        return new Response('Missing message parameter', { status: 400 })
+    }
 
-  try {
-    const image = await openai.createImage({
-      prompt: body.message,
-      n: 1,
-      size: body.size || ImageSizes.SMALL,
-      response_format: "url",
-    })
+    try {
+        const image = await openai.createImage({
+            prompt: body.message,
+            n: 1,
+            size: body.size || ImageSizes.SMALL,
+            response_format: 'url',
+        })
 
-    const json = await image.json()
+        const json = await image.json()
 
-    // TODO: Migrate to returning `json.data` as a whole, instead of
-    // just the first item's URL, so that we can return multiple images
-    const res = { url: json?.data?.[0]?.url ?? "" }
+        // TODO: Migrate to returning `json.data` as a whole, instead of
+        // just the first item's URL, so that we can return multiple images
+        const res = { url: json?.data?.[0]?.url ?? '' }
 
-    return new Response(JSON.stringify(res), {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-  } catch (error: any) {
-    // TODO: Properly handle errors, pop off last user message, or add a message to the chat bot
-    console.error(
-      "Could not access OpenAI, please try again.\n\nError: ",
-      error
-    )
-    return new Response("Could not access OpenAI, please try again.", {
-      status: 400,
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-  }
+        return new Response(JSON.stringify(res), {
+            status: 200,
+            headers: {
+                'content-type': 'application/json',
+            },
+        })
+    } catch (error: any) {
+        // TODO: Properly handle errors, pop off last user message, or add a message to the chat bot
+        console.error(
+            'Could not access OpenAI, please try again.\n\nError: ',
+            error
+        )
+        return new Response('Could not access OpenAI, please try again.', {
+            status: 400,
+            headers: {
+                'content-type': 'application/json',
+            },
+        })
+    }
 }
 
 export default handler
 
 export const config = {
-  runtime: "edge",
+    runtime: 'edge',
 }
