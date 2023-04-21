0171c8d8b3c4ac21dd5981cf57ca46e8b75e34c0
Earle Poole <eepoole4@gmail.com>
2023-04-20 22:46:50 -0500

    lint atom components

--- app/components/atom/LoadingIndicator/LoadingIndicator.tsx
+++ app/components/atom/LoadingIndicator/LoadingIndicator.tsx
@@ -1,38 +1,38 @@
-import styles from "./loadingIndicator.module.css"
+import styles from './loadingIndicator.module.css'
 const LoadingIndicator = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-    >
-      <circle
-        stroke="#fff"
-        fill="#fff"
-        className={styles.spinner_first}
-        cx="4"
-        cy="12"
-        r="3"
-      />
-      <circle
-        stroke="#fff"
-        fill="#fff"
-        className={`${styles.spinner_first} ${styles.spinner_second}`}
-        cx="12"
-        cy="12"
-        r="3"
-      />
-      <circle
-        stroke="#fff"
-        fill="#fff"
-        className={`${styles.spinner_first} ${styles.spinner_third}`}
-        cx="20"
-        cy="12"
-        r="3"
-      />
-    </svg>
-  )
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+        >
+            <circle
+                stroke="#fff"
+                fill="#fff"
+                className={styles.spinner_first}
+                cx="4"
+                cy="12"
+                r="3"
+            />
+            <circle
+                stroke="#fff"
+                fill="#fff"
+                className={`${styles.spinner_first} ${styles.spinner_second}`}
+                cx="12"
+                cy="12"
+                r="3"
+            />
+            <circle
+                stroke="#fff"
+                fill="#fff"
+                className={`${styles.spinner_first} ${styles.spinner_third}`}
+                cx="20"
+                cy="12"
+                r="3"
+            />
+        </svg>
+    )
 }
 
 export default LoadingIndicator
--- app/components/atom/PokemonInput/PokemonInput.tsx
+++ app/components/atom/PokemonInput/PokemonInput.tsx
@@ -1,36 +1,40 @@
-"use client"
+'use client'
 
-import { SyntheticEvent } from "react"
-import { useRouter } from "next/navigation"
-import Button from "../Button/Button"
+import { SyntheticEvent } from 'react'
+import { useRouter } from 'next/navigation'
+import Button from '../Button/Button'
 
-const POKEMON_NAME = "pokemon-name"
+const POKEMON_NAME = 'pokemon-name'
 
 type TargetType<T> = T & {
-  [POKEMON_NAME]: HTMLInputElement
+    [POKEMON_NAME]: HTMLInputElement
 }
 
 const PokemonInput = () => {
-  const router = useRouter()
-  const onSubmitHandler = (e: SyntheticEvent) => {
-    e.preventDefault()
-    const target = e.target as TargetType<typeof e.target>
-    const inputValue = target[POKEMON_NAME].value.toLocaleLowerCase()
-    const cleanInputValue = inputValue.replace(/[^a-z]/g, "")
+    const router = useRouter()
+    const onSubmitHandler = (e: SyntheticEvent) => {
+        e.preventDefault()
+        const target = e.target as TargetType<typeof e.target>
+        const inputValue = target[POKEMON_NAME].value.toLocaleLowerCase()
+        const cleanInputValue = inputValue.replace(/[^a-z]/g, '')
 
-    if (inputValue) {
-      router.push(`/pokemon/${cleanInputValue}`)
+        if (inputValue) {
+            router.push(`/pokemon/${cleanInputValue}`)
+        }
     }
-  }
-  return (
-    <form onSubmit={onSubmitHandler} className="flex flex-col gap-2">
-      <label htmlFor={POKEMON_NAME} className="text-lg font-bold">
-        Look up by name
-      </label>
-      <input className="text-black p-2 rounded" type="text" id={POKEMON_NAME} />{" "}
-      <Button type="submit">Submit</Button>
-    </form>
-  )
+    return (
+        <form onSubmit={onSubmitHandler} className="flex flex-col gap-2">
+            <label htmlFor={POKEMON_NAME} className="text-lg font-bold">
+                Look up by name
+            </label>
+            <input
+                className="text-black p-2 rounded"
+                type="text"
+                id={POKEMON_NAME}
+            />{' '}
+            <Button type="submit">Submit</Button>
+        </form>
+    )
 }
 
 export default PokemonInput
