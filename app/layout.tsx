2023f626c25c545dcc0334e9ab1959468ef518a2
Earle Poole <eepoole4@gmail.com>
2023-04-20 22:43:07 -0500

    lint assets, and root of app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import "../styles/globals.css"
-import Header from "./components/organisms/Header"
-import { LayoutPageProps } from "global"
-import React from "react"
-import { getPokemonList } from "@/utils/cache"
+import '../styles/globals.css'
+import Header from './components/organisms/Header'
+import { LayoutPageProps } from 'global'
+import React from 'react'
+import { getPokemonList } from '@/utils/cache'
 
 export const metadata = {
-  viewport: "width=device-width, initial-scale=1.0",
-  description: "Nested layouts in Next 13",
-  title: "Next 13 Playground",
+    viewport: 'width=device-width, initial-scale=1.0',
+    description: 'Nested layouts in Next 13',
+    title: 'Next 13 Playground',
 }
 
 /**
@@ -15,15 +15,15 @@ export const metadata = {
  * It will wrap every page, including nested pages that have their own layout
  */
 const RootLayout = async (props: LayoutPageProps) => {
-  await getPokemonList()
-  const { children } = props
-  return (
-    <html lang="en" className="min-h-screen flex flex-col">
-      <body className="bg-black text-gray-100 flex flex-col flex-1 h-full">
-        <Header />
-        {children}
-      </body>
-    </html>
-  )
+    await getPokemonList()
+    const { children } = props
+    return (
+        <html lang="en" className="min-h-screen flex flex-col">
+            <body className="bg-black text-gray-100 flex flex-col flex-1 h-full">
+                <Header />
+                {children}
+            </body>
+        </html>
+    )
 }
 export default RootLayout
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
-import Button from "./components/atom/Button"
-import Link from "next/link"
-import { getPokemonList } from "@/utils/cache"
+import Button from './components/atom/Button'
+import Link from 'next/link'
+import { getPokemonList } from '@/utils/cache'
 
 const pages = [
-  { href: "/axios", title: "Axios" },
-  { href: "/pokemon", title: "Pokemon" },
-  { href: "/events", title: "Events" },
-  { href: "/chat", title: "Chat" },
-  { href: "/image", title: "Image" },
+    { href: '/axios', title: 'Axios' },
+    { href: '/pokemon', title: 'Pokemon' },
+    { href: '/events', title: 'Events' },
+    { href: '/chat', title: 'Chat' },
+    { href: '/image', title: 'Image' },
 ]
 
 const Page = async () => {
-  await getPokemonList()
+    await getPokemonList()
 
-  return (
-    <div className="w-screen flex flex-1 items-center flex-col gap-4 p-8">
-      {pages.map(({ href, title }) => (
-        <Link href={href} key={title}>
-          <Button title={title}>{title}</Button>
-        </Link>
-      ))}
-    </div>
-  )
+    return (
+        <div className="w-screen flex flex-1 items-center flex-col gap-4 p-8">
+            {pages.map(({ href, title }) => (
+                <Link href={href} key={title}>
+                    <Button title={title}>{title}</Button>
+                </Link>
+            ))}
+        </div>
+    )
 }
 
 export default Page
