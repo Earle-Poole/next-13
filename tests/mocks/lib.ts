dbc33fb9731582d6e66cfa716f39f045148ac52d
Earle Poole <eepoole4@gmail.com>
2023-04-20 22:39:33 -0500

    linting tests and types

--- tests/mocks/lib.ts
+++ tests/mocks/lib.ts
@@ -1,8 +1,10 @@
-import { NamedAPIResourceList } from 'global';
+import { NamedAPIResourceList } from 'global'
 
 export const mockPokemonList: NamedAPIResourceList = {
-  count: 1,
-  next: null,
-  previous: null,
-  results: [{ name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' }]
+    count: 1,
+    next: null,
+    previous: null,
+    results: [
+        { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+    ],
 }
--- tests/utils.ts
+++ tests/utils.ts
@@ -8,4 +8,3 @@ export function setupFetchStub(data: any) {
         }) as unknown as Promise<Response>
     })
 }
-
